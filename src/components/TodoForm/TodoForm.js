a6472377b48bdbb1dccaf7d9f5c29193481e0482
Saranya Sri <dentist.apassionateprogrammer@gmail.com>
2021-08-19 18:01:30 +0530

    added dynamic style to todo input

--- src/components/TodoForm/TodoForm.js
+++ src/components/TodoForm/TodoForm.js
@@ -2,26 +2,37 @@ import React, { useState } from "react";
 import classes from "./TodoForm.module.css";
 function TodoForm(props) {
   const [newTodo, setNewTodo] = useState("");
+  const [isValid, setIsValid] = useState(true);
   const inputHandler = (e) => {
+    if (e.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setNewTodo(e.target.value);
   };
   const todoSubmitHandler = (e) => {
     e.preventDefault();
+
     const id = Math.random().toString();
     const newlyAddedTodo = { value: newTodo, id: id };
-
-    props.onTodoSubmit(newlyAddedTodo);
-    setNewTodo("");
+    if (newTodo.trim().length === 0) {
+      setIsValid(false);
+    } else {
+      props.onTodoSubmit(newlyAddedTodo);
+      setNewTodo("");
+    }
   };
   return (
-    <form className={classes.todoForm} onSubmit={todoSubmitHandler}>
-      <p>Add New Todo</p>
-      <input
-        required
-        value={newTodo}
-        type="text"
-        onChange={inputHandler}
-      ></input>
+    <form
+      className={`${classes.todoForm} ${!isValid && classes.invalid}`}
+      onSubmit={todoSubmitHandler}
+    >
+      <p> Add New Todo</p>
+      <input value={newTodo} type="text" onChange={inputHandler}></input>
+      {!isValid ? (
+        <p style={{ color: "red", fontWeight: "lighter" }}>
+          *Enter the valid input
+        </p>
+      ) : null}
       <div>
         <button type="submit">ADD TODO</button>
       </div>
